3fcafe331ac34251818dc96e732e87ecb6726cf1
ivan.shagov <shagovivan@mail.ru>
2022-02-10 21:35:52 +0300

    05-virt-04-docker-compose

---
+++ HomeWork/virt-homeworks/05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gkvhj73muchn8k0***"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1ge85lil776vu114***"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8df3fd879m511ap***"
+}
